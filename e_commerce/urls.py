c0dbdb81ae85751012c9d121f671912da0d2bfa8
sinuka <sinukapriv@outlook.com>
2020-09-01 05:46:05 -0300

    create and set -gitignore, e_commerce/__init__.py, settings.py, urls.py, views.py, wsgi.py, template/home_page.html

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from .views import home_page
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+        url('', home_page),
+        url(r'^admin/', admin.site.urls),
 ]
